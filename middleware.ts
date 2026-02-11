df0ffef02c48b54352e2d1f20ef73f09cc0620ef
Axel Blaze <contentstudio1235@gmail.com>
2026-02-11 17:47:55 +0530

    fix(middleware): exclude workbox scripts from auth protection to allow pwa installation

--- middleware.ts
+++ middleware.ts
@@ -105,6 +105,6 @@ export const config = {
          * - favicon.ico (favicon file)
          * Feel free to modify this pattern to include more paths.
          */
-        '/((?!_next/static|_next/image|favicon.ico|manifest\\.webmanifest|sw\\.js|icon|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        '/((?!_next/static|_next/image|favicon.ico|manifest\\.webmanifest|sw\\.js|workbox-.*|icon|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
